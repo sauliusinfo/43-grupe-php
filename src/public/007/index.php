3aa3159ae143aa0797b1caa372ddbe9ec8787196
Saulius <saulius.j@icloud.com>
2023-06-01 22:37:26 +0300

    007

--- src/public/007/index.php
+++ src/public/007/index.php
@@ -1,13 +1,11 @@
 <?php
-  $_ = $_GET['color'] ?? '0';
-  //$color = $_ == 1 ? 'red' : 'grey';
-  if ($_ == 1) {
-    $color = 'red';
-  } elseif ($_ == 0) {
-    $color = 'grey';
-  } else {
-    $color = '#'.$_;
-  }
+  $color = $_GET['color']  ?? 'grey';
+
+  if (preg_match('/^[0-9a-f]{6}$/', $color)) {
+    $color = '#' . $color;
+    } elseif ($color == 1) {
+      $color = 'crimson';
+    }
 ?>
 
 <!DOCTYPE html>
@@ -17,15 +15,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WEB mechanika</title>
+    <style>
+      body {
+        background-color: <?= $color ?>;
+      }
+      a {
+        display: inline-block;
+        color: skyblue;
+        padding: 2px;
+      }
+    </style>
 </head>
-<body style="background-color: <?= $color ?>;">
+<body>
     <h2>:: WEB mechanika ::</h2>
-    <a>1</a><br>
+    <b>1</b><br>
     <a href="http://localhost/007/index.php">Grey</a>
     <a href="http://localhost/007/index.php?color=1">Red</a>
-    <br><a>2</a><br>
-    <a>Spalvos kodas įrašytas ranka: <?php echo $_ ?></a>
-    <br><a>3</a><br>
+    <br><b>2</b><br>
+    <b>Spalvos kodas įrašytas ranka: <?php echo $color ?></b>
+    <br><b>3</b><br>
     <fieldset>
         <legend>GET</legend>
     <form method="get">
@@ -38,5 +46,7 @@
         </div>
     </form>
     </fieldset>
+    <br><b>4</b><br>
+    <br><b>5</b><br>
 </body>
 </html>
