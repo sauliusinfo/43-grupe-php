d3bff6aeb1cea9f38d77c97bbc6564d7858f32fe
Saulius <saulius.j@icloud.com>
2023-06-10 21:54:17 +0300

    Kibiras2

--- src/public/008/Kibiras2.php
+++ src/public/008/Kibiras2.php
@@ -1,8 +1,26 @@
 <?php
 
-class Kibiras2 extends Kibiras1 {
-
-    private static $akmenuKiekisVisuoseKibiruose;
+class Kibiras1 {
     
+    protected $akmenuKiekis = 0;
+    private static $akmenuKiekisVisuoseKibiruose;
 
+    public function prideti1Akmeni() : int
+    {
+        return $this->akmenuKiekis += 1;
+    }
+
+    public function pridetiDaugAkmenu($kiekis) : int
+    {
+        return $this->akmenuKiekis += $kiekis;
+    }
+
+    public function kiekPririnktaAkmenu() : int
+    {
+        return $this->akmenuKiekis;
+    }
+
+    public function akmenuKiekisVisuoseKibiruose() {
+        return self::$akmenuKiekisVisuoseKibiruose;
+    }
 }
--- src/public/008/index.php
+++ src/public/008/index.php
@@ -1,12 +1,13 @@
 <?php
+
 echo '<pre>';
 
 // 1 task
-echo '<div>1</div>';
-
 require __DIR__ . '/Kibiras1.php';
 
-$kibiras = new Kibiras1;
+echo '<div>1</div>';
+
+$kibiras = new Kibiras1();
 
 $kibiras->prideti1Akmeni();
 $kibiras->prideti1Akmeni();
@@ -14,18 +15,24 @@ $kibiras->prideti1Akmeni();
 $kibiras->pridetiDaugAkmenu(5);
 $kibiras->pridetiDaugAkmenu(10);
 
-echo 'is viso ' . $kiekisKibire = $kibiras->kiekPririnktaAkmenu() . PHP_EOL . PHP_EOL;
+echo 'objekto klase - ' . get_class($kibiras) . PHP_EOL;
+echo 'savybes - '; print_r(get_class_vars('Kibiras1'));
+echo 'metodai - '; print_r(get_class_methods('Kibiras1'));
+
+echo 'is viso pririnkom akmenu ' . $kiekisKibire = $kibiras->kiekPririnktaAkmenu() . PHP_EOL;
+echo PHP_EOL;
 
 
 // 2 task
-echo '<div>2</div>';
-
 require __DIR__ . '/Pinigine.php';
 
-$pinigine = new Pinigine;
+echo '<div>2</div>';
+
+$pinigine = new Pinigine();
 
 $pinigine->ideti(2); 
 $pinigine->ideti(5);
+$pinigine->ideti(3);
 
 $pinigine->skaiciuoti();
 
