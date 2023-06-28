7db08945a0a849749e5440b04c34cfd2fd8d3899
Saulius <saulius.j@icloud.com>
2023-06-28 20:04:31 +0300

    bank3

--- src/public/bank3/views/404.php
+++ src/public/bank3/views/404.php
@@ -1,1 +1,8 @@
-<h3>Page Not Found 404</h3>
+<section>
+  <div class="container-fluid hero d-flex flex-column align-items-center py-3 gradient-custom">
+    <h1 class="text-light text-center mt-5 py-3">Page Not Found 404</h1>
+    <div>
+      <img src="<?= URL ?>/img/cat.png" alt="error-404">
+    </div>
+  </div>
+</section>
--- src/public/bank3/views/top.php
+++ src/public/bank3/views/top.php
@@ -4,8 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= $pageTitle ?? 'Untitled' ?></title>
-  <link rel="stylesheet" href="../../app.css">
-  <script type="text/javascript" src="../../app.js"></script>
+  <link rel="stylesheet" href="<?= URL ?>/app.css">
+  <script type="text/javascript" src="<?= URL ?>/app.js" defer></script>
 </head>
 <body>
 
