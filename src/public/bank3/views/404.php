6a2ec81d0556f3400b9fc317e771abbd1c3a6e1c
Saulius <saulius.j@icloud.com>
2023-06-30 12:00:58 +0300

    error404

--- src/public/bank3/views/404.php
+++ src/public/bank3/views/404.php
@@ -1,8 +1,5 @@
 <section>
-  <div class="container-fluid hero d-flex flex-column align-items-center py-3 gradient-custom">
-    <h1 class="text-light text-center mt-5 py-3">Page Not Found 404</h1>
-    <div>
-      <img src="<?= URL ?>/img/cat.png" alt="error-404">
-    </div>
+  <div class="container-fluid hero d-flex flex-column align-items-center justify-content-center py-3 gradient-custom">
+    <h1 class="text-light text-center mt-5 py-3 text-xl gradient-custom-text">Page Not Found 404</h1>
   </div>
 </section>
--- src/public/bank3/views/message.php
+++ src/public/bank3/views/message.php
@@ -1,20 +1,3 @@
-<style>
-  .alert {
-    position: absolute;
-    margin: 75px 15px;
-  }
-  .alert-success {
-    background: #d4edda;
-    border-color: #c3e6cb;
-    color: #155724;
-  }
-  .alert-danger {
-    background: #f8d7da;
-    border-color: #f5c6cb;
-    color: #721c24;
-  }
-</style>
-
 <?php use Bank3\Messages; ?>
 
 <?php if (Messages::ifMessages()) : ?>
